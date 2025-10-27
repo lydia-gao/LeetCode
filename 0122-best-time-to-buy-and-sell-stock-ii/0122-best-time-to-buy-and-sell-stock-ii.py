beac2680de415b664a7dafe65ad2c86a72251326
Lydia Gao <160680760+lydia-gao@users.noreply.github.com>
2025-10-27 14:16:49 -0400

    Time: 0 ms (100%), Space: 13.1 MB (96.29%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        small = prices[0]
+        prev = prices[0]
+        profit = 0
+        for n in prices:
+            if n <= prev:
+                if prev > small:
+                    profit += prev - small
+                small = n
+            else:
+                profit += n - small
+                small = n
+            prev = n
+
+        return profit
