66c9e5624d00460877859a3b42a7c795920838f1
Lydia Gao <160680760+lydia-gao@users.noreply.github.com>
2025-10-27 16:53:48 -0400

    Time: 11 ms (96%), Space: 13.3 MB (78.96%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -4,21 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        goal = len(nums) - 1
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        return goal == 0
 
-        if len(nums) == 1:
-            return True
-
-        for i in range(0, len(nums)):
-            if nums[i] == 0:
-                passd = False
-                need = 1
-                while need <= i:
-                    if nums[i - need] >= need + 1:
-                        passd = True
-                    if i == len(nums) - 1 and nums[i - need] >= need:
-                        passd = True
-                    need += 1
-                if not passd:
-                    return False
-        return True
-                    
