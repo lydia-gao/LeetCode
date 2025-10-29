e42bc20a6afcbe086f34a9655c2e39cb9ab1fe69
Lydia Gao <160680760+lydia-gao@users.noreply.github.com>
2025-10-29 13:07:08 -0400

    Time: 0 ms (100%), Space: 12.9 MB (12.3%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -4,9 +4,19 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
-        l = len(citations)
-        citations.sort()
-        for i in range(l):
-            if (l - i) <= citations[i]:
-                return l - i
+        # l = len(citations)
+        # citations.sort()
+        # for i in range(l):
+        #     if (l - i) <= citations[i]:
+        #         return l - i
+        # return 0
+        n = len(citations)
+        count = [0] * (n + 1)
+        for c in citations:
+            count[min(n, c)] += 1
+        acc = 0
+        for i in range(n, -1, -1):
+            acc += count[i]
+            if i <= acc:
+                return i
         return 0
