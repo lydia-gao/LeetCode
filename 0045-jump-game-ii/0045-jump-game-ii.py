ce179dce41752ba88d4738451db9c8f914c5750d
Lydia Gao <160680760+lydia-gao@users.noreply.github.com>
2025-10-29 10:38:36 -0400

    Time: 7 ms (80.66%), Space: 13.1 MB (69.72%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -4,14 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        jumps = 0
         curr_end = 0
         farthest = 0
-        
+        jumps = 0
+
         for i in range(len(nums) - 1):
             farthest = max(farthest, i + nums[i])
             if i == curr_end:
                 jumps += 1
                 curr_end = farthest
-                
+                if curr_end >= len(nums) - 1:
+                    return jumps
+
         return jumps
