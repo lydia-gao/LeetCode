9d8880f8fc0bfac36ea8616638795da2fbf49522
Lydia Gao <160680760+lydia-gao@users.noreply.github.com>
2025-11-07 12:32:51 -0500

    Time: 33 ms (94.6%), Space: 20.8 MB (63.01%) - LeetHub

--- 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.py
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.py
@@ -1,42 +1,37 @@
 from collections import deque
 
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def maxLevelSum(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
         if not root:
             return 0
-        
+
         max_level = 1
         max_total = root.val
         curr = 1
-        
+        total = 0
+
         q1 = deque([root])
-        
+        q2 = deque()
+
         while q1:
-            total = 0
-            q2 = deque()
-            while q1:
-                top = q1.popleft()
-                total += top.val
-                if top.left:
-                    q2.append(top.left)
-                if top.right:
-                    q2.append(top.right)
-            
-            if total > max_total:
-                max_total = total
-                max_level = curr
-                
-            q1 = q2
-            curr += 1
-        
+            top = q1.popleft()
+            total += top.val
+
+            if top.left:
+                q2.append(top.left)
+            if top.right:
+                q2.append(top.right)
+
+            # End of current level
+            if not q1:
+                if total > max_total:
+                    max_total = total
+                    max_level = curr
+
+                # Prepare for next level
+                q1 = q2
+                q2 = deque()
+                total = 0
+                curr += 1
+
         return max_level
