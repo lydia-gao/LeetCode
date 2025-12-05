48c65d4da7bdba6105950e80046a7fd7531495c1
Lydia Gao <160680760+lydia-gao@users.noreply.github.com>
2025-12-05 13:35:20 -0500

    Time: 9 ms (57.75%), Space: 28.4 MB (10.86%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -7,12 +7,11 @@ class Solution:
         for right in nums:
             curr += right
             count += 1
-            if curr >= target:
+
+            while curr >= target:
                 res = min(res, count)
-                while curr >= target:
-                    res = min(res, count)
-                    count -= 1
-                    curr -= nums[left]
-                    left += 1
+                count -= 1
+                curr -= nums[left]
+                left += 1
 
         return 0 if res == float('inf') else res
