5edaa18d12e959d5104719b6d8ed45cb4a2397d0
Lydia Gao <160680760+lydia-gao@users.noreply.github.com>
2026-05-23 16:11:05 -0400

    Time: 41 ms (85.08%), Space: 19.2 MB (19.06%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,11 @@
 class Solution:
     def fib(self, n: int) -> int:
+        dp = [0, 1]
         def recur(num: int) -> int:
-            if num == 0:
-                return 0
-            elif num == 1:
-                return 1
+            if num < len(dp):
+                return dp[num]
             else:
-                return recur(num - 1) + recur(num - 2)
-        return recur(n)   
+                res = recur(num - 1) + recur(num - 2)
+                dp.append(res)
+                return res
+        return recur(n)
