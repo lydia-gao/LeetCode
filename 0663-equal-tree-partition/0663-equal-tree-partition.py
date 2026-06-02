f6c96595e70facc580d08ec741dd9085f06a7b04
Lydia Gao <160680760+lydia-gao@users.noreply.github.com>
2026-06-02 00:32:19 -0400

    Time: 3 ms (82.19%), Space: 22.8 MB (32.88%) - LeetHub

--- 0663-equal-tree-partition/0663-equal-tree-partition.py
+++ 0663-equal-tree-partition/0663-equal-tree-partition.py
@@ -1,29 +1,21 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def checkEqualTree(self, root: Optional[TreeNode]) -> bool:
-        found = False
-        def count(root):
-            if not root:
+        sums = []
+
+        def dfs(node):
+            if not node:
                 return 0
-            return root.val + count(root.left) + count(root.right)
-        total = count(root)
+
+            curr = node.val + dfs(node.left) + dfs(node.right)
+            sums.append(curr)
+            return curr
+
+        total = dfs(root)
+
+        # remove root's total sum because we cannot cut above root
+        sums.pop()
+
         if total % 2 != 0:
             return False
-        def dfs(root):
-            nonlocal found
-            if not root:
-                return 0
-            if found:
-                return 0
-            acc = root.val + dfs(root.left) + dfs(root.right)
-            if acc == total // 2:
-                found = True
-            return acc
-        dfs(root.left)
-        dfs(root.right)
-        return found
+
+        return total // 2 in sums
